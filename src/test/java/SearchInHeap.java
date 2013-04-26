e556ed18ad22b5d4880894bd06a9d1219d2999a0
Simon Lundström <simmel@soy.se>
2013-04-26 23:47:44 +0200

    Remove debug prints

--- src/test/java/SearchInHeap.java
+++ src/test/java/SearchInHeap.java
@@ -10,13 +10,10 @@ import org.w3c.dom.NodeList;
 public class SearchInHeap {
   @After public void searchInHeap() throws IOException, InterruptedException, XPathExpressionException {
     String pid = ManagementFactory.getRuntimeMXBean().getName().replaceAll("@.*", "");
-    System.out.println(pid);
 
     String tmp = System.getProperty("temporaryDir");
     String hprof = tmp + "/" + pid + ".hprof";
-    System.out.println(hprof);
     String jhatPort = Integer.toString(7000 + Integer.parseInt(System.getProperty("org.gradle.test.worker")));
-    System.out.println(jhatPort);
 
     Runtime.getRuntime().exec(new String[] {"jmap", String.format("-dump:format=b,file=%s", hprof), pid}).waitFor();
     String jhat = "jhat -port " + jhatPort + " " + hprof;
