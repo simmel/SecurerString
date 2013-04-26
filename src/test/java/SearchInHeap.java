d1f8760cddb6863eabc61a9e1884f801ab82e436
Simon Lundström <simmel@soy.se>
2013-04-26 22:42:42 +0200

    Run jhat on the dump

--- src/test/java/SearchInHeap.java
+++ src/test/java/SearchInHeap.java
@@ -1,6 +1,7 @@
 import org.junit.After;
 import java.lang.management.*;
 import java.io.*;
+import java.net.*;
 
 public class SearchInHeap {
   @After public void searchInHeap() throws IOException, InterruptedException {
@@ -14,7 +15,18 @@ public class SearchInHeap {
     System.out.println(jhatPort);
 
     Runtime.getRuntime().exec(new String[] {"jmap", String.format("-dump:format=b,file=%s", hprof), pid}).waitFor();
+    String jhat = "jhat -port " + jhatPort + " " + hprof;
+    Process p=Runtime.getRuntime().exec(jhat);
 
+    BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream()));
+    String line=reader.readLine();
+    while(!line.matches("Server is ready."))
+    {
+      line=reader.readLine();
+    }
+    Thread.sleep(1000);
+
+    Runtime.getRuntime().exec(new String[] {"pkill", "-f", jhat}).waitFor();
     new File(hprof).delete();
   }
 }
