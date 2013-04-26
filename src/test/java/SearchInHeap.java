09a3bb8e46da52b7e5917c73b001b1d11d977ad7
Simon Lundström <simmel@soy.se>
2013-04-26 22:18:19 +0200

    Dump the heap with jmap

--- src/test/java/SearchInHeap.java
+++ src/test/java/SearchInHeap.java
@@ -1,8 +1,9 @@
 import org.junit.After;
 import java.lang.management.*;
+import java.io.*;
 
 public class SearchInHeap {
-  @After public void searchInHeap() {
+  @After public void searchInHeap() throws IOException, InterruptedException {
     String pid = ManagementFactory.getRuntimeMXBean().getName().replaceAll("@.*", "");
     System.out.println(pid);
 
@@ -11,5 +12,9 @@ public class SearchInHeap {
     System.out.println(hprof);
     String jhatPort = Integer.toString(7000 + Integer.parseInt(System.getProperty("org.gradle.test.worker")));
     System.out.println(jhatPort);
+
+    Runtime.getRuntime().exec(new String[] {"jmap", String.format("-dump:format=b,file=%s", hprof), pid}).waitFor();
+
+    new File(hprof).delete();
   }
 }
