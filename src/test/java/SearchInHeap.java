046d177191d44f1ee43e568a6436e4334c1503a3
Simon Lundström <simmel@soy.se>
2013-04-26 22:12:20 +0200

    Print hprof dump file and jhat port

--- src/test/java/SearchInHeap.java
+++ src/test/java/SearchInHeap.java
@@ -5,5 +5,11 @@ public class SearchInHeap {
   @After public void searchInHeap() {
     String pid = ManagementFactory.getRuntimeMXBean().getName().replaceAll("@.*", "");
     System.out.println(pid);
+
+    String tmp = System.getProperty("temporaryDir");
+    String hprof = tmp + "/" + pid + ".hprof";
+    System.out.println(hprof);
+    String jhatPort = Integer.toString(7000 + Integer.parseInt(System.getProperty("org.gradle.test.worker")));
+    System.out.println(jhatPort);
   }
 }
