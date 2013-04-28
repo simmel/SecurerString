9232a485c8504c64a2eae6222b5828f8fbd9431a
Simon Lundström <simmel@soy.se>
2013-04-28 17:56:08 +0200

    Use try so we can clean up in finally

--- src/test/java/SearchInHeap.java
+++ src/test/java/SearchInHeap.java
@@ -10,41 +10,48 @@ import org.w3c.dom.NodeList;
 public class SearchInHeap {
   @After public void searchInHeap() throws IOException, InterruptedException, XPathExpressionException {
     String pid = ManagementFactory.getRuntimeMXBean().getName().replaceAll("@.*", "");
-
     String tmp = System.getProperty("temporaryDir");
     String hprof = tmp + "/" + pid + ".hprof";
     String jhatPort = Integer.toString(7000 + Integer.parseInt(System.getProperty("org.gradle.test.worker")));
-
-    Runtime.getRuntime().exec(new String[] {"jmap", String.format("-dump:format=b,file=%s", hprof), pid}).waitFor();
     String jhat = "jhat -port " + jhatPort + " " + hprof;
-    Process p=Runtime.getRuntime().exec(jhat);
+    try {
+      System.out.println("Dumping heap with jmap");
+      Runtime.getRuntime().exec(new String[] {"jmap", String.format("-dump:format=b,file=%s", hprof), pid}).waitFor();
 
-    BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream()));
-    String line=reader.readLine();
-    while(!line.matches("Server is ready."))
-    {
-      line=reader.readLine();
+      System.out.println("Starting jhat");
+      Process p=Runtime.getRuntime().exec(jhat);
+
+      BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream()));
+      String line=reader.readLine();
+      while(!line.matches("Server is ready."))
+      {
+        line=reader.readLine();
+      }
+      System.out.println("jhat ready!");
+      Thread.sleep(1000);
+
+      System.out.println("Running OQL query...");
+      URL oqlUrl = new URL("http://localhost:" + jhatPort + "/oql/?query=" + URLEncoder.encode("select a.toString() from [C a where /really_unique_string/(a.toString())", "UTF-8"));
+      URLConnection oql = oqlUrl.openConnection();
+      BufferedReader in = new BufferedReader(new InputStreamReader(oql.getInputStream()));
+      String inputLine;
+      String xml = "";
+
+      while ((inputLine = in.readLine()) != null)
+        xml += inputLine;
+      in.close();
+
+      // What I don't do just not to add an external dependency...
+      xml = xml.replaceAll("(\\w+)=([^'\"][^ ]+?)([^'\"])", "$1='$2'$3");
+      xml = xml.replaceAll("\000", "");
+      String xpath = "//table[@border=1]//td";
+      XPath xPath = XPathFactory.newInstance().newXPath();
+      NodeList nodeList = (NodeList) xPath.evaluate(xpath, new InputSource(new StringReader(xml)), XPathConstants.NODESET);
+      assertEquals("Secret found!", 0, nodeList.getLength());
     }
-    Thread.sleep(1000);
-
-    URL oqlUrl = new URL("http://localhost:" + jhatPort + "/oql/?query=" + URLEncoder.encode("select a.toString() from [C a where /really_unique_string/(a.toString())", "UTF-8"));
-    URLConnection oql = oqlUrl.openConnection();
-    BufferedReader in = new BufferedReader(new InputStreamReader(oql.getInputStream()));
-    String inputLine;
-    String xml = "";
-
-    while ((inputLine = in.readLine()) != null)
-      xml += inputLine;
-    in.close();
-
-    xml = xml.replaceAll("(\\w+)=([^'\"][^ ]+?)([^'\"])", "$1='$2'$3");
-    xml = xml.replaceAll("\000", "");
-    String xpath = "//table[@border=1]//td";
-    XPath xPath = XPathFactory.newInstance().newXPath();
-    NodeList nodeList = (NodeList) xPath.evaluate(xpath, new InputSource(new StringReader(xml)), XPathConstants.NODESET);
-    assertEquals(0, nodeList.getLength());
-
-    Runtime.getRuntime().exec(new String[] {"pkill", "-f", jhat}).waitFor();
-    new File(hprof).delete();
+    finally {
+      Runtime.getRuntime().exec(new String[] {"pkill", "-f", jhat}).waitFor();
+      new File(hprof).delete();
+    }
   }
 }
